97e7219d42822a374d35ab8e97327cefb7b2f588
meryem08 <meriem.mansouri08@gmail.com>
2023-09-12 14:46:48 +0100

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    const words = tutorial.split(' ');
+    const capitalizedWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
 }
+
+console.log(titleCased());
+
